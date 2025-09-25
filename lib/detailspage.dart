b1dd162e6bc7ab39f71a13406405b65a9015685a
Sundarampal <sundrampal61@gmail.com>
2025-09-25 11:41:48 +0530

    Update detailspage.dart

--- lib/detailspage.dart
+++ lib/detailspage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'detailspage.dart';
+
 class Detailspage extends StatelessWidget {
 final String title;
 final String details;
@@ -17,18 +18,7 @@ final String details;
     );
   }
  }
-// class app extends StatefulWidget {
-//   const app({super.key});
-//
-//   @override
-//   State<app> createState() => _appState();
-// }
-//
-// class _appState extends State<app> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold( appBar: AppBar(title: Text('sundram'),),
-//     );
-//   }
-// }
-//
+ class Questionpage {
+  static List<Widget>questionwidgets = [];
+
+ }
